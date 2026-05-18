7ce741c2e2d33c539e1ef43161344e99ed1146c7
naichaniuniu <naichaniuniu@github.com>
2026-05-18 19:06:58 +0800

    修复欠款数据：同步dashboard_final.json正确数据（1593.91万）

---
+++ update_final_json.py
@@ -0,0 +1,36 @@
+import json
+
+with open('dashboard_data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+with open('dashboard_final.json', 'r', encoding='utf-8') as f:
+    final = json.load(f)
+
+depts = data['depts']
+dept_debt = data['dept_debt']
+debt_by_age = data['debt_by_dept_age']
+debt_cycle = data['debt_cycle']
+receipt = data['receipt']
+
+for d in final['dept_data']:
+    dept = d['dept']
+    d['total_debt'] = round(dept_debt.get(dept, 0), 2)
+    d['d30'] = round(debt_by_age.get(dept, {}).get('30天内', 0), 2)
+    d['d30_90'] = round(debt_by_age.get(dept, {}).get('30-90天', 0), 2)
+    d['d90_180'] = round(debt_by_age.get(dept, {}).get('90-180天', 0), 2)
+    d['d180'] = round(debt_by_age.get(dept, {}).get('180天以上', 0), 2)
+    d['cycle'] = round(debt_cycle.get(dept, 0), 1)
+    d['collect'] = round(receipt.get(dept, 0), 2)
+
+final['total']['debt'] = round(sum(dept_debt.values()), 2)
+final['total']['d30'] = round(data['age_dist']['30天内'], 2)
+final['total']['d30_90'] = round(data['age_dist']['30-90天'], 2)
+final['total']['d90_180'] = round(data['age_dist']['90-180天'], 2)
+final['total']['d180'] = round(data['age_dist']['180天以上'], 2)
+final['total']['collect'] = round(sum(receipt.values()), 2)
+
+with open('dashboard_final.json', 'w', encoding='utf-8') as f:
+    json.dump(final, f, ensure_ascii=False, indent=2)
+
+print('dashboard_final.json updated')
+print(f'Debt total: {final["total"]["debt"]}')
